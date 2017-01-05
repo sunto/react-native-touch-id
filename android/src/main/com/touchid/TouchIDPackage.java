6de347c11820e646b226c1f3de5f0c78c17f6d88
Sunny To <sunny@laterolabs.com>
2017-01-05 10:53:38 -0800

    permissioning

--- android/src/main/com/touchid/TouchIDPackage.java
+++ android/src/main/com/touchid/TouchIDPackage.java
@@ -6,5 +6,29 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TouchIDPackage implements ReactPackage {
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+      List<NativeModule> modules = new ArrayList<>();
+
+      modules.add(new TouchIDModule(reactContext));
+
+      return modules;
+  }
+
+  @Override
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+      return Collections.emptyList();
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+      return Collections.emptyList();
+  }
+
 }
